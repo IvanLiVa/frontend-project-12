98b48ec6bfe3bbfa443df26c2f81b78326d6c880
IvaтLiVa <litvinovivanvadimovich@gmail.com>
2024-12-03 09:54:57 +0400

    Added Rollbar integration for React, errors are sent to the service

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout/MainLayout.jsx';
 import './styles/styles.css';
 import Login from './pages/LoginPage/LoginPage.jsx';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 import NotFound from './pages/NotFoundPage/NotFoundPage.jsx';
 import useConnectSocket from './hooks/useConnectSocket.js';
 import Signup from './pages/signup/Signup.jsx';
@@ -10,28 +11,39 @@ import NavbarChat from './components/Chat/navbar.jsx';
 import 'react-toastify/dist/ReactToastify.css';
 import CustomToastContainer from './components/toastContainer.jsx';
 
+const rollbarConfig = {
+  accessToken: 'aaa72a41996849d0abcead8546323fcb',
+  environment: 'production',
+};
+
 const App = () => {
   useConnectSocket();
+
   return (
-    <Router>
-      <div className="d-flex flex-column h-100">
-        <NavbarChat />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <ProtectedRoute>
-                <MainLayout />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="login" element={<Login />} />
-          <Route path="signup" element={<Signup />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-        <CustomToastContainer />
-      </div>
-    </Router>
+    <RollbarProvider config={rollbarConfig}>
+      <ErrorBoundary>
+        {' '}
+        <Router>
+          <div className="d-flex flex-column h-100">
+            <NavbarChat />
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <ProtectedRoute>
+                    <MainLayout />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="login" element={<Login />} />
+              <Route path="signup" element={<Signup />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+            <CustomToastContainer />
+          </div>
+        </Router>
+      </ErrorBoundary>
+    </RollbarProvider>
   );
 };
 
